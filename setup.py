905366e192abb81517984957e7f50041a1c42159
Cesar Renno-Costa <rennocosta@neuro.ufrn.br>
2026-08-23 14:11:42 -0300

    v0.2.17: Update 1elo, 2eloFS, and 2eloOD comparison notebooks with consolidated 3-tuple benchmark datasets

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="multielo-football",
-    version="0.2.16",
+    version="0.2.17",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.20.0",
