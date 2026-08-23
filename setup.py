6c7dd43cfa8f7a9d848ad1af280253a2f0ebf56e
Cesar Renno-Costa <rennocosta@neuro.ufrn.br>
2026-08-23 14:46:26 -0300

    v0.2.20: Add intel-256 and intel-512 with exact 1:1:4:8 cluster partition allocation ratio

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="multielo-football",
-    version="0.2.19",
+    version="0.2.20",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.20.0",
