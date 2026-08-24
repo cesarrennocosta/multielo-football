076943d67a6dc7f3eb7c89340dfda4a1076660f2
Cesar Renno-Costa <rennocosta@neuro.ufrn.br>
2026-08-24 19:22:06 -0300

    v0.2.29: Add submit_2elood_priority.sh dedicated launcher for 2-Elo Offense-Defense jobs in requested priority order (2eloODG -> 2eloOD -> 2eloODK -> 2eloODGc)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="multielo-football",
-    version="0.2.28",
+    version="0.2.29",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.20.0",
