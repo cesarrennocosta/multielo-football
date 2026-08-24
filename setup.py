774cc602615cb2ebe8f97d0534b1cc8e3b931339
Cesar Renno-Costa <rennocosta@neuro.ufrn.br>
2026-08-24 19:24:39 -0300

    v0.2.30: Add submit_2elood_all.sh master launcher for ALL 6 2-Elo Offense-Defense systems (576 SLURM jobs)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="multielo-football",
-    version="0.2.29",
+    version="0.2.30",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.20.0",
