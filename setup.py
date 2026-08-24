35ea3b0121e0943b20fe43b6ed925292024637af
Cesar Renno-Costa <rennocosta@neuro.ufrn.br>
2026-08-24 07:42:23 -0300

    v0.2.23: Enqueue seed trials for eloratings.net and 1eloG optimal hotspots in Optuna search

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="multielo-football",
-    version="0.2.22",
+    version="0.2.23",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.20.0",
