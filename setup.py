ba6cd34a140b45fd7c0d6709605b81254fdd05b1
Cesar Renno-Costa <rennocosta@neuro.ufrn.br>
2026-08-25 20:56:20 -0300

    v0.2.35: Add submit_all_missing_incomplete.sh master launcher for exact 234 missing Optuna jobs (< 500 trials) across rating systems and inertia scaling

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="multielo-football",
-    version="0.2.34",
+    version="0.2.35",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.20.0",
