89bc1874a7a38aecf5b05472971bb1f7c3ef9552
Cesar Renno-Costa <rennocosta@neuro.ufrn.br>
2026-08-30 00:15:41 +0300

    v0.2.59: Enqueue 2eloODGc best parameters across all 96 2eloODG DBs and create submit_2eloodg_continuation.sh master launcher for +300 trials (800 total trials)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="multielo-football",
-    version="0.2.58",
+    version="0.2.59",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.20.0",
