b6ca8e4af2a6e809981d3282347c5bd16961ecd1
Cesar Renno-Costa <rennocosta@neuro.ufrn.br>
2026-08-29 14:15:43 +0300

    v0.2.39: Auto-evaluate formula baselines and fix relative percentage formatting in merge_all_db_evaluations.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="multielo-football",
-    version="0.2.38",
+    version="0.2.39",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.20.0",
