4b3def1e942c52772b4c9edca756812ae9add7cb
Cesar Renno-Costa <rennocosta@neuro.ufrn.br>
2026-08-29 16:59:21 +0300

    v0.2.45: Fix idxmin indexing and generate 3-panel figure and Jupyter notebook for 1eloC rating scale inertia experiment (alpha in [0.1, 1.9])

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="multielo-football",
-    version="0.2.44",
+    version="0.2.45",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.20.0",
