d0178d8d81291db64c5ec7c9975e8170b4d10336
Cesar Renno-Costa <rennocosta@neuro.ufrn.br>
2026-08-29 17:04:20 +0300

    v0.2.46: Fix string escaping and safe NaN indexing in inertia_1eloc_analysis.ipynb (verified 100% error-free execution)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="multielo-football",
-    version="0.2.45",
+    version="0.2.46",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.20.0",
