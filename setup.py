5a51869b69d37e7e75ceef11d225219acfe7b6fe
Cesar Renno-Costa <rennocosta@neuro.ufrn.br>
2026-08-29 22:16:27 +0300

    v0.2.50: Update optuna_train_eras.py so all 5 partitions (Eras 1-4 & Control) use exact 11,545 match subsampling equally spaced across time

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="multielo-football",
-    version="0.2.49",
+    version="0.2.50",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.20.0",
