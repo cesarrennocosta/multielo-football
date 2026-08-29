ddcc994c883f4838a268fb321a891151192ba3ce
Cesar Renno-Costa <rennocosta@neuro.ufrn.br>
2026-08-30 00:07:25 +0300

    v0.2.56: Consolidate 2eloODG transferred parameters across all 5 metrics and update 2eloOD_comparisson.ipynb

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="multielo-football",
-    version="0.2.55",
+    version="0.2.56",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.20.0",
